a12e2803fafee12e864a0b955b59e2c1020eefb1
krikun_sa <krikun_sa@ayax.ru>
2019-07-31 10:03:34 +0300

    F-01 Change checkbox

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-module.exports = {
+/* module.exports = {
   root: true,
   extends: [ 'riophae/vue' ],
   plugins: [ 'react' ],
@@ -28,4 +28,4 @@ module.exports = {
     "linebreak-style": 0,
     'no-unused-vars': ['error'],
   },
-}
+} */
