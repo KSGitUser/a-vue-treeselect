fc5555d58cf3fc26a4a213eab1381adcacf8dee1
krikun_sa <krikun_sa@ayax.ru>
2019-08-02 13:36:22 +0300

    Added label attr to css ::after

--- docs/components/MoreFeatures.vue
+++ docs/components/MoreFeatures.vue
@@ -16,56 +16,76 @@
       :limit="3"
       :max-height="200"
       v-model="value"
-      />
+    />
     <pre class="result">{{ value }}</pre>
     <p>
-      <label><input type="checkbox" v-model="multiple">Multi-select</label>
-      <label><input type="checkbox" v-model="clearable">Clearable</label>
-      <label><input type="checkbox" v-model="searchable">Searchable</label>
-      <label><input type="checkbox" v-model="disabled">Disabled</label>
+      <label>
+        <input type="checkbox" v-model="multiple" />Multi-select
+      </label>
+      <label>
+        <input type="checkbox" v-model="clearable" />Clearable
+      </label>
+      <label>
+        <input type="checkbox" v-model="searchable" />Searchable
+      </label>
+      <label>
+        <input type="checkbox" v-model="disabled" />Disabled
+      </label>
     </p>
     <p>
-      <label><input type="checkbox" v-model="openOnClick">Open on click</label>
-      <label><input type="checkbox" v-model="openOnFocus">Open on focus</label>
+      <label>
+        <input type="checkbox" v-model="openOnClick" />Open on click
+      </label>
+      <label>
+        <input type="checkbox" v-model="openOnFocus" />Open on focus
+      </label>
     </p>
     <p>
-      <label><input type="checkbox" v-model="clearOnSelect">Clear on select</label>
-      <label><input type="checkbox" v-model="closeOnSelect">Close on select</label>
+      <label>
+        <input type="checkbox" v-model="clearOnSelect" />Clear on select
+      </label>
+      <label>
+        <input type="checkbox" v-model="closeOnSelect" />Close on select
+      </label>
     </p>
     <p>
-      <label><input type="checkbox" v-model="alwaysOpen">Always open</label>
-      <label><input type="checkbox" v-model="appendToBody">Append to body</label>
+      <label>
+        <input type="checkbox" v-model="alwaysOpen" />Always open
+      </label>
+      <label>
+        <input type="checkbox" v-model="appendToBody" />Append to body
+      </label>
     </p>
   </div>
 </template>
 
 <script>
-  import { generateOptions } from './utils'
+import { generateOptions } from "./utils";
 
-  export default {
-    data: () => ({
-      multiple: true,
-      clearable: true,
-      searchable: true,
-      disabled: false,
-      openOnClick: true,
-      openOnFocus: false,
-      clearOnSelect: true,
-      closeOnSelect: false,
-      alwaysOpen: false,
-      appendToBody: false,
-      value: [ 'a' ],
-      options: generateOptions(2, 3),
-    }),
+export default {
+  data: () => ({
+    multiple: true,
+    clearable: true,
+    searchable: true,
+    disabled: false,
+    openOnClick: true,
+    openOnFocus: false,
+    clearOnSelect: true,
+    closeOnSelect: false,
+    alwaysOpen: false,
+    appendToBody: false,
+    value: ["a"],
+    options: generateOptions(2, 3)
+  }),
 
-    watch: {
-      multiple(newValue) {
-        if (newValue) {
-          this.value = this.value ? [ this.value ] : []
-        } else {
-          this.value = this.value[0]
-        }
-      },
-    },
+  watch: {
+    multiple(newValue) {
+      if (newValue) {
+        this.value = this.value ? [this.value] : [];
+      } else {
+        this.value = this.value[0];
+      }
+    }
   }
+};
 </script>
